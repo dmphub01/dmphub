e10727a3d6fd77470e489a4bb22d4dff0dc0bfa2
dmphub01 <dmphub01@gmail.com>
2016-06-07 21:09:06 +0530

    Uploaded on 07-06-16 at 9.10pm

---
+++ scripts/controllers/uploadphone.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name DMPHUB.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Controller of the moreYourApp
+ */
+angular.module('app')
+  .controller('UploadphoneController', function ($scope) {
+    $scope.awesomeThings = [
+      
+    ];
+  });
